6fab1d8026abe1d588882a8ec7bf28d6c59c83de
John Martin C. Cabacungan <123425952+QJm-1@users.noreply.github.com>
2026-04-11 12:16:50 +0800

    Update solenoid.py

--- cashew_dashboard/actuators/solenoid.py
+++ cashew_dashboard/actuators/solenoid.py
@@ -1,1 +1,18 @@
+import RPi.GPIO as GPIO
+import time
 
+RELAY_PIN = 24
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(RELAY_PIN, GPIO.OUT)
+GPIO.output(RELAY_PIN, GPIO.HIGH)  # relay off by default
+
+def mist_on():
+    GPIO.output(RELAY_PIN, GPIO.LOW)
+
+def mist_off():
+    GPIO.output(RELAY_PIN, GPIO.HIGH)
+
+def mist_pulse(seconds=2):
+    mist_on()
+    time.sleep(seconds)
+    mist_off()
