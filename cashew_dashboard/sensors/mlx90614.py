a3c3598273ce8bc8ea1a4136a3eb2fa7ddfb0bcd
John Martin C. Cabacungan <123425952+QJm-1@users.noreply.github.com>
2026-04-11 12:07:58 +0800

    Update mlx90614.py

--- cashew_dashboard/sensors/mlx90614.py
+++ cashew_dashboard/sensors/mlx90614.py
@@ -1,1 +1,12 @@
+import board
+import busio
+import adafruit_mlx90614
 
+i2c = busio.I2C(board.SCL, board.SDA)
+mlx = adafruit_mlx90614.MLX90614(i2c)
+
+def get_nozzle_temp():
+    return round(mlx.object_temperature, 2)
+
+def get_ambient_temp():
+    return round(mlx.ambient_temperature, 2)
